c43461a8e354fc8e90c4c3befe61c9678d6d0aa3
iluvyw <anhoangpham2001@gmail.com>
2021-08-05 20:37:57 +0700

    Modify ingredients textview

--- app/src/main/java/com/example/foodrecommendation/fragment/FoodDetailFragment.kt
+++ app/src/main/java/com/example/foodrecommendation/fragment/FoodDetailFragment.kt
@@ -6,8 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.foodrecommendation.R
+import kotlinx.android.synthetic.main.fragment_food_detail.*
 
 class FoodDetailFragment : Fragment() {
+
+    val list = mutableListOf<String>("bun bo","banh canh","cha gio")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,4 +19,17 @@ class FoodDetailFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_food_detail, container, false)
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        tvIngredient.text = displayIngredients(list)
+    }
+}
+
+fun displayIngredients(list: MutableList<String>): String {
+    var ingredients = ""
+    for (i in list.indices) {
+        ingredients = ingredients + list[i] + '\n'
+    }
+    return ingredients
 }
