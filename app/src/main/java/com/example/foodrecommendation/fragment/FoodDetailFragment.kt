bc57cdf446fd51a7c73fe3c0b4054ea8afe8dcf8
iluvyw <anhoangpham2001@gmail.com>
2021-08-10 19:19:02 +0700

    first build

--- app/src/main/java/com/example/foodrecommendation/fragment/FoodDetailFragment.kt
+++ app/src/main/java/com/example/foodrecommendation/fragment/FoodDetailFragment.kt
@@ -1,5 +1,6 @@
 package com.example.foodrecommendation.fragment
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,7 +11,30 @@ import kotlinx.android.synthetic.main.fragment_food_detail.*
 
 class FoodDetailFragment : Fragment() {
 
-    val list = mutableListOf<String>("bun bo","banh canh","cha gio")
+    val list = mutableListOf<String>(
+        "We firstly need 400g of tomatoes. Use a knife and lightly cut an X on top of the tomatoes",
+        "Then put them all in a boiling pot of water. Leave it to cook for 30 seconds then take it out",
+        "Next, carve out the seeds of the tomato",
+        "After that, place your tomatoes neatly into the blender and dismember them. You can also mince them instead.",
+        "After that, set it aside",
+        "Next I'll cut up some onions and cry and then blame it on the onions",
+        "Put all the onion into a bowl",
+        "Next, cut 20g of bell pepper. Bell peppers aren't spicy so don't worry if you can't eat spicy food. You can replace it with carrots or just skip this ingredient",
+        "Now that you're done with the ingredients prepping phase, you can start cooking. I'll start with 20g of butter in a pan. You can also use olive oil",
+        "I'll throw in 10g of minced garlic into the pan after the butter is melted",
+        "Add in your chopped bell pepper and onion. Mix it repeatedly until all of it starts to turn golden and smells super good",
+        "Then add 150g of minced beef into the pan. Pan-fry it for about 2-4 minutes",
+        "Next, pour in all the blended tomatoes from earlier into the pan",
+        "And add in 50ml of water into the pan as well",
+        "Mix it well then add in all the spices: ―tsp of salt and 2 tsp of sugar. You can change this to taste and the sourness of the tomatoes themselves",
+        "Next is 50g of ketchup (not tomato sauce, don't mix them up and 1 tsp of dried oregano",
+        "Lastly is a bit of grounded pepper and some chopped culantro (not cilantro)",
+        "Mix it well then close the lid and turn down the heat to low and wait for about 20min, until the sauce becomes denser",
+        "You can put in more spices if you want since the sauce is basically done now. When you're done with that, pour it on your pasta.",
+    )
+    val ingredientsList = mutableListOf<String>("400g Tomatoes","40g Onion","40g Bell pepper",
+        "20g Unsalted butter","10g Garlic","150g Beef","1/2Tsp Salt","2Tsp Sugar","50g Ketchup",
+        "1Tsp Dried Oregano","Black pepper","10g Parsley")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,16 +44,27 @@ class FoodDetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_food_detail, container, false)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        tvIngredient.text = displayIngredients(list)
+    override fun onStart() {
+        super.onStart()
+        tvIngredient.text = displayItems(ingredientsList)
+        tvCookSteps.text = displayItems(list)
+        var count = 0
+        tvFoodImage.setOnClickListener {
+            count = (count + 1) % 3
+            tvFoodImage.setImageResource(getResourceId("spaghetti",count,requireContext()))
+        }
     }
 }
 
-fun displayIngredients(list: MutableList<String>): String {
-    var ingredients = ""
+fun displayItems(list: MutableList<String>): String {
+    var items = ""
     for (i in list.indices) {
-        ingredients = ingredients + list[i] + '\n'
+        items = items + (i+1).toString() + ". " + list[i] + "\n"
     }
-    return ingredients
+    return items
+}
+
+fun getResourceId(filename: String, count: Int, context: Context): Int{
+    val id: Int = context.resources.getIdentifier(filename+"_"+count.toString(),"drawable",context.packageName)
+    return id
 }
